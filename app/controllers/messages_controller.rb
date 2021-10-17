46f30d45951b3f1a2df1f7e29b019765305d279f
Oskar <oskar.madej.wroc@gmail.com>
2021-10-17 16:19:25 +0200

    +edit and destroy meths

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
   def index
     @messages = Message.all.order("created_at DESC")
     # list in descending order (must be plural)
   end
   
   def show
-    @message = Message.find(params[:id])
+    
   end
 
   def new
@@ -21,9 +22,31 @@ class MessagesController < ApplicationController
     end
   end 
 
+  def edit
+    #edit only finds and shows the post
+  end 
+
+  def update
+    #update saves new params of the post 
+    if @message.update(message_params)
+      redirect_to message_path
+    else 
+      render 'edit'
+    end
+  end 
+
+  def destroy
+    @message.destroy 
+    redirect_to root_path
+  end
+
   private 
 
   def message_params
     (params.require(:message).permit(:title, :description))
   end
+
+  def find_message
+    @message = Message.find(params[:id])
+  end 
 end
